3058dfdbf4cd00867a364fa3b19c2d4b23794da7
jorgemhtdev <jorge@jorgemht.dev>
2023-03-30 19:31:07 +0200

    Initial Commit

---
+++ Beers/BeersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeersApp.swift
+//  Beers
+//
+//  Created by jorgemht on 30/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BeersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
