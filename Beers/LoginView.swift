998f87916380e9a60258bcfc951b3f8c08c8a3f8
jorgemhtdev <jorge@jorgemht.dev>
2023-04-02 18:46:03 +0200

    [Added] - ViewThatFits

--- Beers/LoginView.swift
+++ Beers/LoginView.swift
@@ -61,7 +61,7 @@ struct LoginView: View {
                 .padding(.horizontal)
 
                 Spacer()
-                
+
                 Button {
 
                 } label: {
@@ -83,4 +83,121 @@ struct LoginView_Previews: PreviewProvider {
         LoginView()
     }
 }
+
+// Adaptive Layouts => Con ViewThatFits podemos seleccionar uno de varios diseños posibles en función de lo que se ajuste al espacio de pantalla disponible. Una forma fantástica de asegurarnos de que nuestra app se vea bien en cualquier pantalla.
+
+//NavigationStack {
+//
+//    ViewThatFits {
+//
+//        HStack(spacing: 25) {
+//            BeerShape()
+//                .stroke()
+//                .frame(width: 175, height: 175)
+//                .foregroundStyle(
+//                    LinearGradient(gradient:
+//                                    Gradient(
+//                                        colors: [.orangeXA, .orangeStrongXA]),
+//                                   startPoint: .topLeading,
+//                                   endPoint: .bottomTrailing
+//                                  )
+//                )
+//                .cornerRadius(10)
+//                .padding(.top)
+//
+//            VStack(alignment: .center, spacing: 30) {
+//                VStack(alignment: .leading, spacing: 15) {
+//                    Text("Username")
+//                        .font(.body.bold())
+//
+//                    TextField("Username", text: $username)
+//                        .padding()
+//                        .textFieldBordered()
+//                        .font(.body)
+//
+//                }
+//                .padding(.horizontal)
+//
+//                VStack(alignment: .leading, spacing: 15) {
+//                    Text("Password")
+//                        .font(.body.bold())
+//
+//                    SecureField("Password", text: $password)
+//                        .padding()
+//                        .textFieldBordered()
+//                        .font(.body)
+//                }
+//                .padding(.horizontal)
+//
+//                Button {
+//
+//                } label: {
+//                    Text("Sign In")
+//                }
+//                .buttonStyle(MainButtonStyle())
+//            }
+//            .frame(minWidth: 500)
+//        }
+//
+//        VStack(spacing: 50) {
+//
+//            BeerShape()
+//                .stroke()
+//                .frame(width: 175, height: 175)
+//                .foregroundStyle(
+//                    LinearGradient(gradient:
+//                                    Gradient(
+//                                        colors: [.orangeXA, .orangeStrongXA]),
+//                                   startPoint: .topLeading,
+//                                   endPoint: .bottomTrailing
+//                                  )
+//                )
+//                .cornerRadius(10)
+//                .padding(.top)
+//
+//            Spacer()
+//
+//            VStack(alignment: .leading, spacing: 15) {
+//                Text("Username")
+//                    .font(.body.bold())
+//
+//                TextField("Username", text: $username)
+//                    .padding()
+//                    .textFieldBordered()
+//                    .font(.body)
+//
+//            }
+//            .padding(.horizontal)
+//
+//            VStack(alignment: .leading, spacing: 15) {
+//                Text("Password")
+//                    .font(.body.bold())
+//
+//                SecureField("Password", text: $password)
+//                    .padding()
+//                    .textFieldBordered()
+//                    .font(.body)
+//            }
+//            .padding(.horizontal)
+//
+//            Spacer()
+//
+//            Button {
+//
+//            } label: {
+//                Text("Sign In")
+//            }
+//            .buttonStyle(MainButtonStyle())
+//        }
+//        .navigationDestination(
+//            isPresented: $isLoggedIn) {
+//                MainView()
+//            }
+//            .padding()
+//
+//    }
+//    }
+
+
+// Before ViewThatFits => GeometryReader
 
